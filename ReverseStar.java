6035697621cf1d671af1c2f30512d87333d8248a
suvankar jana <112017165+janasubha@users.noreply.github.com>
2022-12-09 22:31:07 +0530

    Add files via upload

---
+++ ReverseStar.java
@@ -0,0 +1,15 @@
+import java.util.*;
+	public class ReverseStar{
+		public static void main(String[] args){
+			for(int i=15;i>=0;i--){
+				for(int j=1;j<=15;j++){
+					if(j<=i)
+					System.out.print("*@*\t");
+					else
+					System.out.print("");
+				}
+			System.out.print("\n");
+                         System.out.print("\n");
+			}
+	}
+}
