305ac04ebb4081b23f64ddf8081cd720e833e4a4
suvankar jana <112017165+janasubha@users.noreply.github.com>
2022-12-19 22:10:07 +0530

    Add files via upload

---
+++ Core java Program/PreAndPostIncrementDemo.java
@@ -0,0 +1,13 @@
+public class PreAndPostIncrementDemo {
+
+	public static void main(String[] args) {
+		int x=10;
+		//int y=x++;//postincrement
+		int y=++x;//preincrement
+		System.out.println(y);
+		System.out.println(x);
+		
+
+	}
+
+}
